b12052a3b7aa8ba9292aef3006c19d4a5a4f527c
Muhannad Abbasi <58631937+Muhannad-Abbasi@users.noreply.github.com>
2020-12-11 12:27:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,16 +9,13 @@ fetch(url).then(
 ).then(
     function(data){
         let threeCountry = [];
-        console.log(data[0])
 
         for( i = 0; i < 3; i++){
             let rand = Math.floor(Math.random() * data.length);
-            console.log(rand)
 
             threeCountry.push(
-                new Country(data[rand].name, data[rand].timezones ,data[rand].flag)
+                new Country(data[rand].name, data[rand].timezones, data[rand].flag)
             )
-            console.log(threeCountry)
         }
         
 
@@ -27,7 +24,6 @@ fetch(url).then(
             c.displayImg();
             c.displyTid();
         }
-        console.log(c)
         
 
     }
@@ -41,7 +37,7 @@ function Country(_namn, _tidsZon, _img){
 
 Country.prototype.displayNamn = function(){
     let contName = document.querySelector('h1');
-    contName.innerText = this.name;
+    contName.innerHTML = this.name;
 }
 
 Country.prototype.displayImg = function(){
@@ -52,8 +48,8 @@ Country.prototype.displayImg = function(){
 Country.prototype.displyTid = function(){
     let contTid = document.querySelector('h3');
     let date = new Date()
-    let hour = date.getHours();
-    let min = date.getMinutes();
+    let hour = date.getUTCHours();
+    let min = date.getUTCMinutes();
     contTid.innerHTML = `${hour}:${min}`
 
 }
